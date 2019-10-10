6b6d8463cb8b30cf565b9b6a1dc238084ec0cb86
arnaud <arnaud@websiteburo.com>
2019-10-10 14:04:32 +0200

    add Grid

---
+++ classes/Grid.py
@@ -0,0 +1,24 @@
+class Grid(object):
+    """
+    Grid = La grille de jeu
+    height: Dim H
+    width: Dim W
+    Points: 2D List of points
+    """
+    
+    def __init__(self, width, height):
+        self.height = height
+        self.width = width
+        for 
+        self.PowPoints = []
+
+    def set_point(self, PowPoint):
+        
+        self.PowPoints[PowPoint.posX
+    
+        
+    #def spawn_station(self, SpawnStation):
+    #    self.PowStation = SpawnStation        
+        
+    #def pow_point(self, PowPoint):
+    #    self.PowPoints.append(PowPoint)
